1d5dd37b49219d2c2cb27747f57d4156c1dd40ab
hunkguo <guoyao@gmail.com>
2020-03-04 17:56:18 +0800

    helloworld调试成功，测试用例通过，主要解决测试用例引用包的问题，随着错误一个个解决，不乱引用包，导致莫名其妙的问题

--- springbootdemo/src/main/java/com/hunk/springbootdemo/HelloController.java
+++ springbootdemo/src/main/java/com/hunk/springbootdemo/HelloController.java
@@ -1,4 +1,14 @@
 package com.hunk.springbootdemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index(){
+        return "Hello World!";
+    }
 }
