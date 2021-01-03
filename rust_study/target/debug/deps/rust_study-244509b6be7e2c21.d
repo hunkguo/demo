b80bec75eab544d19a972eecfaa4a9b70cc0ce15
hunkguo <guoyao@gmail.com>
2021-01-03 19:30:23 +0800

    rust

---
+++ rust_study/target/debug/deps/rust_study-244509b6be7e2c21.d
@@ -0,0 +1,5 @@
+/home/hunk/workspaces/demo/rust_study/target/debug/deps/rust_study-244509b6be7e2c21: src/main.rs
+
+/home/hunk/workspaces/demo/rust_study/target/debug/deps/rust_study-244509b6be7e2c21.d: src/main.rs
+
+src/main.rs:
---
+++ rust_study/target/debug/rust_study.d
@@ -0,0 +1,1 @@
+/home/hunk/workspaces/demo/rust_study/target/debug/rust_study: /home/hunk/workspaces/demo/rust_study/src/main.rs
